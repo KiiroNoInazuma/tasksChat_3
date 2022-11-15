db7887d895ec3677d0d621da40b0531d6dd1f4ed
John Rey <adioskid@mail.ru>
2022-11-16 01:10:58 +0300

    Homework#6.1

--- src/Number_5.java
+++ src/Number_5.java
@@ -1,5 +1,14 @@
 public class Number_5 {
+    static final String fullName = "Ivanov Ivan Ivanovich";
+
     public static void main(String[] args) {
-        
+        String lastName = fullName.substring(0, fullName.indexOf(' '));
+        String firstName = fullName.substring(fullName.indexOf(' '), fullName.lastIndexOf(' '));
+        String middleName = fullName.substring(fullName.lastIndexOf(' '));
+        System.out.println("Имя сотрудника - " + firstName);
+        System.out.println("Фамилия сотрудника - " + lastName);
+        System.out.println("Отчество сотрудника - " + middleName);
+
+
     }
 }
